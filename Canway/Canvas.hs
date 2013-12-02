e4fd87b2fc39d576f0dc03e2e36f71fbf9c6e743
RyanGlScott <ryan.gl.scott@ku.edu>
2013-12-02 17:42:28 -0600

    More examples, bigger grids

--- Canway/Canvas.hs
+++ Canway/Canvas.hs
@@ -16,40 +16,40 @@ type FontSize = Int
 
 drawButton :: X -> Y -> Width -> Height -> FontSize -> String -> Canvas ()
 drawButton x y w h fs capt = do
-	beginPath()
-	fillStyle("#C6FF87")
-	fillRect(x, y, w, h)
-	fillStyle("black")
-	font(show fs ++ "px Arial")
-	fillText(capt, x + fromIntegral fs*1.2, y + fromIntegral fs*1.2)
-	closePath()
+    beginPath()
+    fillStyle("#C6FF87")
+    fillRect(x, y, w, h)
+    fillStyle("black")
+    font(show fs ++ "px Arial")
+    fillText(capt, x + fromIntegral fs*1.2, y + fromIntegral fs*1.2)
+    closePath()
 
 drawLines :: Width -> XOffset -> Height -> YOffset -> Width -> XPadding -> Height -> YPadding -> Canvas ()
 drawLines sw sx sh sy cw cwd ch chd  = do
-	mapM_ (\ y -> drawLine sx y (sw+cwd) chd) [sy,(ch+sy)..(sh+sy)]
-	mapM_ (\ x -> drawLine x sy cwd (sh+chd)) [sx,(cw+sx)..(sw+sx)]
+    mapM_ (\ y -> drawLine sx y (sw+cwd) chd) [sy,(ch+sy)..(sh+sy)]
+    mapM_ (\ x -> drawLine x sy cwd (sh+chd)) [sx,(cw+sx)..(sw+sx)]
 
 drawLine :: X -> Y -> Width -> Height -> Canvas ()
 drawLine x y w h = do
-	beginPath()
-	fillStyle("#808080")
-	fillRect(x, y, w, h)
-	fill()
-	closePath()
+    beginPath()
+    fillStyle("#808080")
+    fillRect(x, y, w, h)
+    fill()
+    closePath()
 
 clearCell :: X -> XOffset -> Y -> YOffset -> Width -> XPadding -> Height -> YPadding -> Cell -> Bool -> Canvas ()
 clearCell x dx y dy cw cwd ch chd c heatSeeking = if (not heatSeeking || c == Alive)
-	then clearRect(dx + cwd + cw*x, dy + chd + ch*y, cw-cwd, ch-chd)
-	else return ()
+                                                  then clearRect(dx + cwd + cw*x, dy + chd + ch*y, cw-cwd, ch-chd)
+                                                  else return ()
 
 drawCell :: X -> XOffset -> Y -> YOffset -> Width -> XPadding -> Height -> YPadding -> Cell -> Canvas ()
 drawCell x dx y dy cw cwd ch chd c = if c == Alive then do
-	beginPath()
-	fillStyle("black")
-	fillRect(dx + cwd + cw*x, dy + chd + ch*y, cw-cwd, ch-chd)
-	fill()
-	closePath()
-					     else return ()
+    beginPath()
+    fillStyle("black")
+    fillRect(dx + cwd + cw*x, dy + chd + ch*y, cw-cwd, ch-chd)
+    fill()
+    closePath()
+                                     else return ()
 
 clearGrid :: XOffset -> YOffset -> Width -> XPadding -> Height -> YPadding -> Bool -> Grid -> Canvas ()
 clearGrid sx sy cw cwd ch chd heatSeeking g = imapGridM_ (\ x y c -> clearCell (fromIntegral x) sx (fromIntegral y) sy cw cwd ch chd c heatSeeking) g
@@ -59,69 +59,74 @@ drawGrid sw sx sh sy cw cwd ch chd g = imapGridM_ (\ x y c -> drawCell (fromInte
 
 chooseGrid :: Int -> Int -> Context -> IO ()
 chooseGrid x y context = do
-	let grid = fillGrid x y Dead
-	(w, h) <- send context size -- The canvas width/height
-	let (sw, sh) = (w, h*0.9) -- The actual grid width/height. The height is reduced to make room for captions.
-	let (sx, sy) = ((w - sw)/2, 0.0) -- The grid x-/y-offset. The grid is centered horizontally.
-	let (gw, gh) = dimens grid -- The number of cells in a row/column
-	let (cw, ch) = (sw/fromIntegral gw, sh/fromIntegral gh) -- The width/height of a single cell
-	let (cwd, chd) = (cw*0.10, ch*0.10) -- The width/height of a grid line
-	let (bx, by, bw, bh) = (w*0.44, h*0.92, 100, 50) -- Button size
-	let fs = 16 -- Button font size
-	let capt = "Animate" -- Button caption
+    let grid = fillGrid x y Dead
+    (w, h) <- send context size -- The canvas width/height
+    let (sw, sh) = (w, h*0.9) -- The actual grid width/height. The height is reduced to make room for captions.
+    let (sx, sy) = ((w - sw)/2, 0.0) -- The grid x-/y-offset. The grid is centered horizontally.
+    let (gw, gh) = dimens grid -- The number of cells in a row/column
+    let (cw, ch) = (sw/fromIntegral gw, sh/fromIntegral gh) -- The width/height of a single cell
+    let (cwd, chd) = (cw*0.10, ch*0.10) -- The width/height of a grid line
+    let (bx, by, bw, bh) = (w*0.44, h*0.92, 100, 50) -- Button size
+    let fs = 16 -- Button font size
+    let capt = "Animate" -- Button caption
 
-	send context $ drawLines sw sx sh sy cw cwd ch chd
-	send context $ drawButton bx by bw bh fs capt
-	
-	let getRectCoord start end coord = if start <= coord && coord < end then Just coord else Nothing
-	let pointToCorner (x, y) = do
-		let nx = floor $ (fromIntegral x - sx) / cw
-		let ny = floor $ (fromIntegral y - sy) / ch
-		x' <- getRectCoord 0 gw nx
-		y' <- getRectCoord 0 gh ny
-		Just (x',y')
-	
-	let eventHandler g = do
-		send context $ clearGrid sx sy cw cwd ch chd False g
-		send context $ drawGrid sw sx sh sy cw cwd ch chd g
-		
-		event <- send context $ readEvent MouseDown
-		case jsMouse event of
-			Nothing -> eventHandler g
-			Just (x', y') -> case pointToCorner (x', y') of
-				Nothing -> let button = do
-						bx' <- getRectCoord bx (bx+bw) (fromIntegral x')
-						by' <- getRectCoord by (by+bh) (fromIntegral y')
-						Just (bx', by')
-					in case button of
-						Nothing -> eventHandler g
-						Just _ -> do
-							send context $ clearRect(bx, by, bw, bh)
-							animateGrid g context
-				Just (ex, ey) -> case updateToggle g ex ey of
-					Nothing -> eventHandler g
-					Just g' -> eventHandler g'
-	
-	eventHandler grid
+    send context $ drawLines sw sx sh sy cw cwd ch chd
+    send context $ drawButton bx by bw bh fs capt
+    
+    let getRectCoord :: Ord a => a -> a -> a -> Maybe a
+        getRectCoord start end coord = if start <= coord && coord < end then Just coord else Nothing
+    let pointToCorner :: (Int, Int) -> Maybe (Int, Int)
+        pointToCorner (x, y) = do
+        let nx = floor $ (fromIntegral x - sx) / cw
+        let ny = floor $ (fromIntegral y - sy) / ch
+        x' <- getRectCoord 0 gw nx
+        y' <- getRectCoord 0 gh ny
+        Just (x',y')
+        
+    let eventHandler :: Grid -> IO ()
+        eventHandler g = do
+        send context $ clearGrid sx sy cw cwd ch chd False g
+        send context $ drawGrid sw sx sh sy cw cwd ch chd g
+        
+        event <- send context $ readEvent MouseDown
+        case jsMouse event of
+             Nothing -> eventHandler g
+             Just (x', y') -> case pointToCorner (x', y') of
+                                   Nothing -> let button = do
+                                                      bx' <- getRectCoord bx (bx+bw) (fromIntegral x')
+                                                      by' <- getRectCoord by (by+bh) (fromIntegral y')
+                                                      Just (bx', by')
+                                              in case button of
+                                                      Nothing -> eventHandler g
+                                                      Just _ -> do
+                                                            send context $ clearRect(bx, by, bw, bh)
+                                                            animateGrid g context
+                                   Just (ex, ey) -> case updateToggle g ex ey of
+                                                         Nothing -> eventHandler g
+                                                         Just g' -> eventHandler g'
+    
+    eventHandler grid
 
 animateGrid :: Grid -> Context -> IO ()
 animateGrid grid context = do
-	(w, h) <- send context size -- The canvas width/height
-	let (sw, sh) = (w, h*0.9) -- The actual grid width/height. The height is reduced to make room for captions.
-	let (sx, sy) = ((w - sw)/2, 0.0) -- The grid x-/y-offset. The grid is centered horizontally.
-	let (gw, gh) = dimens grid -- The number of cells in a row/column
-	let (cw, ch) = (sw/fromIntegral gw, sh/fromIntegral gh) -- The width/height of a single cell
-	let (cwd, chd) = (cw*0.10, ch*0.10) -- The width/height of a grid line
-	
-	send context $ drawLines sw sx sh sy cw cwd ch chd
-	
-	let animateGridCells g = do
-		send context $ drawGrid sw sx sh sy cw cwd ch chd g
-		threadDelay $ 1 * 1000 * 100
-		send context $ clearGrid sx sy cw cwd ch chd True g
-		animateGridCells $ tick g
-	animateGridCells grid
+    (w, h) <- send context size -- The canvas width/height
+    let (sw, sh) = (w, h*0.9) -- The actual grid width/height. The height is reduced to make room for captions.
+    let (sx, sy) = ((w - sw)/2, 0.0) -- The grid x-/y-offset. The grid is centered horizontally.
+    let (gw, gh) = dimens grid -- The number of cells in a row/column
+    let (cw, ch) = (sw/fromIntegral gw, sh/fromIntegral gh) -- The width/height of a single cell
+    let (cwd, chd) = (cw*0.10, ch*0.10) -- The width/height of a grid line
+    
+    send context $ drawLines sw sx sh sy cw cwd ch chd
+    
+    let animateGridCells :: Grid -> IO ()
+        animateGridCells g = do
+        send context $ drawGrid sw sx sh sy cw cwd ch chd g
+        threadDelay $ 1 * 1000 * 50
+        send context $ clearGrid sx sy cw cwd ch chd True g
+        animateGridCells $ tick g
+    
+    animateGridCells grid
 
 main :: IO ()
-main = blankCanvas 3000 $ chooseGrid 15 15
--- main = blankCanvas 3000 $ animateGrid gosperGliderGun
+--main = blankCanvas 3000 $ chooseGrid 15 15
+main = blankCanvas 3000 $ animateGrid infinite2Big
--- Canway/Grid.hs
+++ Canway/Grid.hs
@@ -11,10 +11,10 @@ data Cell = Dead | Alive deriving (Eq)
 data Grid = Grid (V.Vector (V.Vector Cell)) deriving (Eq)
 
 instance Show Cell where
-	show c = show $ toChar c
+    show c = show $ toChar c
 
 instance Show Grid where
-	show (Grid v) = intercalate "\n" $ V.toList $ V.map (V.toList . (V.map toChar)) v
+    show (Grid v) = intercalate "\n" $ V.toList $ V.map (V.toList . (V.map toChar)) v
 
 toChar :: Cell -> Char
 toChar Dead = ' '
@@ -41,9 +41,9 @@ updateToggle g x y = index g x y >>= \ c -> update g x y (toggle c)
 
 update :: Grid -> Int -> Int -> Cell -> Maybe Grid
 update (Grid v) x y c = do
-	row <- (v !? y)
-	row !? x
-	Just $ Grid $ v // [(y, row // [(x, c)])]
+    row <- (v !? y)
+    row !? x
+    Just $ Grid $ v // [(y, row // [(x, c)])]
 
 fillGrid :: Int -> Int -> Cell -> Grid
 fillGrid x y = Grid . V.replicate y . V.replicate x
@@ -53,26 +53,33 @@ fillGrid x y = Grid . V.replicate y . V.replicate x
 fromList :: [[Char]] -> Grid
 fromList [] = error "Grid must be non-empty"
 fromList cs = Grid $ V.fromList $ map (V.fromList . map spaceDead) cs
-	where spaceDead c = if isSpace c then Dead else Alive
+    where spaceDead c = if isSpace c then Dead else Alive
 
 -- The number of living neighbors
 neighbors :: Grid -> Int -> Int -> Int
 neighbors g x y = length $ filter (== Alive) [fromJust nc | dy <- [-1,0,1], dx <- [-1,0,1], dx /= 0 || dy /= 0, let nc = index g (x+dx) (y+dy), isJust nc]
 
 rules :: Cell -> Grid -> Int -> Int -> Cell
-rules c g x y = case c of Alive -> if n < 2 then Dead			-- Rule 1
-				   else if n == 2 || n == 3 then Alive	-- Rule 2
-				   else Dead				-- Rule 3
-			  Dead -> if n == 3 then Alive else Dead	-- Rule 4
-	where n = neighbors g x y
+rules c g x y = case c of Alive -> if n < 2 then Dead                           -- Rule 1
+                                            else if n == 2 || n == 3 then Alive -- Rule 2
+                                                                     else Dead  -- Rule 3
+                          Dead -> if n == 3 then Alive else Dead	        -- Rule 4
+    where n :: Int
+          n = neighbors g x y
 
 tick :: Grid -> Grid
 tick g@(Grid v) = Grid $ V.imap (\ y row -> V.imap (\ x c -> rules c g x y) row) v
 
 gen :: Int -> Grid -> Grid
 gen n g
-	| n <= 0 = g
-	| otherwise = (gen $! n-1) $! tick g
+    | n <= 0 = g
+    | otherwise = (gen $! n-1) $! tick g
+
+mapGrid :: (Cell -> Cell) -> Grid -> Grid
+mapGrid f (Grid v) = Grid $ V.map (V.map f) v
+
+imapGrid :: (Int -> Int -> Cell -> Cell) -> Grid -> Grid
+imapGrid f (Grid v) = Grid $ V.imap (\ y -> V.imap (\ x -> f x y)) v
 
 mapGridM_ :: Monad m => (Cell -> m a) -> Grid -> m ()
 mapGridM_ f (Grid v) = V.sequence_ $ V.map (V.mapM f) v
@@ -82,60 +89,179 @@ imapGridM_ f (Grid v) = V.sequence_ $ V.imap (\ y -> V.sequence_ . V.imap (\ x -
 
 period :: Grid -> Int
 period g = periodAux g g 1 where
-	periodAux og ng t = let ng' = tick ng in
-			 if og == ng' then t else periodAux og ng' t+1
+    periodAux og ng t = let ng' = tick ng
+                        in if og == ng'
+                              then t
+                              else periodAux og ng' t+1
+
+intersectGrids :: Grid -> Grid -> Grid
+intersectGrids a b = let blank = fillGrid (max (width a) (width b)) (max (height a) (height b)) Dead
+                     in imapGrid (\ x y _ -> intersectCells x y) blank
+    where intersectCells :: Int -> Int -> Cell
+          intersectCells x y = let maybeC1 = index a x y
+                                   maybeC2 = index b x y
+                                   maybeAnd = maybeC1 >>= \ c1 -> maybeC2 >>= \ c2 -> if (c1 == Alive) && (c2 == Alive) then return Alive else return Dead
+                               in if (isJust maybeAnd) then (fromJust maybeAnd) else Dead
+
+unionGrids :: Grid -> Grid -> Grid
+unionGrids a b = let blank = fillGrid (max (width a) (width b)) (max (height a) (height b)) Dead
+                 in imapGrid (\ x y _ -> unionCells x y) blank
+    where unionCells :: Int -> Int -> Cell
+          unionCells x y = let maybeC1 = index a x y
+                               maybeC2 = index b x y
+                               maybeOr = maybeC1 >>= \ c1 -> maybeC2 >>= \ c2 -> if (c1 == Alive) || (c2 == Alive) then return Alive else return Dead
+                           in if (isJust maybeOr) then (fromJust maybeOr) else Dead
+
+shiftGrid :: Int -> Int -> Grid -> Grid
+shiftGrid x y g = let blank = fillGrid (width g + x) (height g + y) Dead
+                  in imapGrid (\ x' y' _ -> getCell g (width g - x') (height g - y')) blank
+    where getCell :: Grid -> Int -> Int -> Cell
+          getCell h x'' y'' = case index h x'' y'' of
+                                   Just c  -> c
+                                   Nothing -> Dead
+
+embiggen :: Grid -> Grid
+embiggen g = shiftGrid 25 25 $ unionGrids g $ fillGrid 25 25 Dead
 
 blinker :: Grid
-blinker = fromList [	"     ",
-			"  X  ",
-			"  X  ",
-			"  X  ",
-			"     "]
+blinker = fromList [    "     ",
+                        "  X  ",
+                        "  X  ",
+                        "  X  ",
+                        "     "]
 
 toad :: Grid
-toad = fromList [	"      ",
-			"      ",
-			"  XXX ",
-			" XXX  ",
-			"      ",
-			"      "]
+toad = fromList [       "      ",
+                        "      ",
+                        "  XXX ",
+                        " XXX  ",
+                        "      ",
+                        "      "]
 
 beacon :: Grid
-beacon = fromList [	"      ",
-			" XX   ",
-			" X    ",
-			"    X ",
-			"   XX ",
-			"      "]
-	
+beacon = fromList [     "      ",
+                        " XX   ",
+                        " X    ",
+                        "    X ",
+                        "   XX ",
+                        "      "]
+
 pulsar :: Grid
-pulsar = fromList [	"                 ",
-			"                 ",
-			"    XXX   XXX    ",
-			"                 ",
-			"  X    X X    X  ",
-			"  X    X X    X  ",
-			"  X    X X    X  ",
-			"    XXX   XXX    ",
-			"                 ",
-			"    XXX   XXX    ",
-			"  X    X X    X  ",
-			"  X    X X    X  ",
-			"  X    X X    X  ",
-			"                 ",
-			"    XXX   XXX    ",
-			"                 ",
-			"                 "]
+pulsar = fromList [     "                 ",
+                        "                 ",
+                        "    XXX   XXX    ",
+                        "                 ",
+                        "  X    X X    X  ",
+                        "  X    X X    X  ",
+                        "  X    X X    X  ",
+                        "    XXX   XXX    ",
+                        "                 ",
+                        "    XXX   XXX    ",
+                        "  X    X X    X  ",
+                        "  X    X X    X  ",
+                        "  X    X X    X  ",
+                        "                 ",
+                        "    XXX   XXX    ",
+                        "                 ",
+                        "                 "]
+
+clock :: Grid
+clock = fromList [ "              ",
+                   "       XX     ",
+                   "       XX     ",
+                   "              ",
+                   "     XXXX     ",
+                   " XX X  X X    ",
+                   " XX X X  X    ",
+                   "    X X  X XX ",
+                   "    X    X XX ",
+                   "     XXXX     ",
+                   "              ",
+                   "     XX       ",
+                   "     XX       ",
+                   "              "]
 
 gosperGliderGun :: Grid
-gosperGliderGun = fromList [	"                                      ",
-				"                         X            ",
-				"                       X X            ",
-				"             XX      XX            XX ",
-				"            X   X    XX            XX ",
-				" XX        X     X   XX               ",
-				" XX        X   X XX    X X            ",
-				"           X     X       X            ",
-				"            X   X                     ",
-				"             XX                       ",
-				"                                      "]
+gosperGliderGun = fromList [    "                                      ",
+                                "                         X            ",
+                                "                       X X            ",
+                                "             XX      XX            XX ",
+                                "            X   X    XX            XX ",
+                                " XX        X     X   XX               ",
+                                " XX        X   X XX    X X            ",
+                                "           X     X       X            ",
+                                "            X   X                     ",
+                                "             XX                       ",
+                                "                                      "]
+                                
+gosperGliderGunBig :: Grid
+gosperGliderGunBig = unionGrids gosperGliderGun (fillGrid 50 50 Dead)
+
+infinite1 :: Grid
+infinite1 = fromList [  "          ",
+                        "       X  ",
+                        "     X XX ",
+                        "     X X  ",
+                        "     X    ",
+                        "   X      ",
+                        " X X      ",
+                        "          "]
+
+infinite1Big :: Grid
+infinite1Big = embiggen infinite1
+
+infinite2 :: Grid
+infinite2 = fromList [  "       ",
+                        " XXX X ",
+                        " X     ",
+                        "    XX ",
+                        "  XX X ",
+                        " X X X ",
+                        "       "]
+
+infinite2Big :: Grid
+infinite2Big = embiggen infinite2
+
+methusela :: Grid
+methusela = fromList [  "          ",
+                        " XX   X X ",
+                        " XX    X  ",
+                        "       X  ",
+                        "          "]
+
+methuselaBig :: Grid
+methuselaBig = embiggen methusela
+
+maxFill :: Grid
+maxFill = fromList [    "                             ",
+                        "                   X         ",
+                        "                  XXX        ",
+                        "             XXX    XX       ",
+                        "            X  XXX  X XX     ",
+                        "           X   X X  X X      ",
+                        "           X    X X X X XX   ",
+                        "             X    X X   XX   ",
+                        " XXXX     X X    X   X XXX   ",
+                        " X   XX X XXX XX         XX  ",
+                        " X     XX     X              ",
+                        "  X  XX X  X  X XX           ",
+                        "        X X X X X X     XXXX ",
+                        "  X  XX X  X  X  XX X XX   X ",
+                        " X     XX   X X X   XX     X ",
+                        " X   XX X XX  X  X  X XX  X  ",
+                        " XXXX     X X X X X X        ",
+                        "           XX X  X  X XX  X  ",
+                        "              X     XX     X ",
+                        "  XX         XX XXX X XX   X ",
+                        "   XXX X   X    X X     XXXX ",
+                        "   XX   X X    X             ",
+                        "   XX X X X X    X           ",
+                        "      X X  X X   X           ",
+                        "     XX X  XXX  X            ",
+                        "       XX    XXX             ",
+                        "        XXX                  ",
+                        "         X                   ",
+                        "                             "]
+
+maxFillBig :: Grid
+maxFillBig = shiftGrid 20 20 $ unionGrids maxFill $ fillGrid 40 40 Dead
